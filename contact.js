ed726597b3537299a350e03d763377d53404c3c6
larrazajuanp <larrazajuanp@gmail.com>
2020-05-20 15:44:39 -0300

    Iniciamos el pryecto de la landing

---
+++ contact.js
@@ -0,0 +1,9 @@
+let email = document.getElementById('input-email');
+let input = document.getElementById('CTA');
+
+const getEmail = () => {
+    const value = email.value;
+    console.log(value);
+}
+
+getEmail();
